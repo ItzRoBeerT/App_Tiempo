4118c22971198b27c1a47c47da224c94f5ca7064
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2023-02-09 14:42:45 +0100

    aaa

---
+++ App_Tiempo/MVVM/ViewModels/TiempoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Tiempo.MVVM.ViewModels
+{
+    public class TiempoViewModel
+    {
+
+        private async Task<Location> GetCoordinatesAsync(string address)
+        {
+            var locations = await Geocoding.GetLocationsAsync(address);
+            var location = locations?.FirstOrDefault();
+
+            return location;
+        }
+    }
+}
